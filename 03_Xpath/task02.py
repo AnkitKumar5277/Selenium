2ef8015d921491ebf45c605c59a3d5420b8cfb32
Ankit Knight <121275934+AnkitKumar5277@users.noreply.github.com>
2025-08-11 16:26:29 +0530

    Add files via upload

---
+++ 03_Xpath/task02.py
@@ -0,0 +1,28 @@
+import allure
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+@allure.title("Practicing Xpath")
+@allure.description("TC1 - Exploring Xpath")
+
+def test_xpath_chrome():
+    driver = webdriver.Chrome()
+    driver.get("https://katalon-demo-cura.herokuapp.com/")
+    make_app_btn = driver.find_element(By.XPATH,"//a[contains(text(),'Make')")
+    make_app_btn.click()
+
+    # text() -> exact match
+    #partial text() - contains
+    # //a[contains(text(),'Make Appointment')]
+    # //a[contains(text(),'Appointment')]
+    # //a[contains(text(),'Make')]
+    # //a[contains(text(),'Ma')]
+    # //a[contains(text(),'App')] - this may fail if there 1 or more a tag with app
+    # <a rel="https:/google.com"/>
+    # //a[starts-with(text(),'Make')]
+    time.sleep(2)
+    driver.quit()
+
