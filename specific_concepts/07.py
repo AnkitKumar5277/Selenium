817e99aa6c77e890ad68feb65dace891cc37ae85
Ankit Knight <121275934+AnkitKumar5277@users.noreply.github.com>
2025-09-12 09:48:02 +0530

    Add files via upload

---
+++ specific_concepts/07.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+import time
+
+driver=webdriver.Edge()
+driver.get("https://www.selenium.dev/selenium/web/formPage.html")
+dropdown = driver.find_element("name","selectomatic")
+
+select = Select(dropdown)
+select.select_by_visible_text("One")
+time.sleep(1)
+
+select.select_by_index(2)
+time.sleep(1)
+
+select.select_by_value("fouth")
+time.sleep(1)
+
+driver.quit()
