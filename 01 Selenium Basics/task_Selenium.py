84ae23134753609066ea827dff060f9159cfb1e1
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-11-23 21:44:31 +0530

    Update task_Selenium.py

--- 01 Selenium Basics/task_Selenium.py
+++ 01 Selenium Basics/task_Selenium.py
@@ -12,7 +12,7 @@ import  pytest
 
 @allure.title("Verify that the title and current url of espocrm.com is as expected.")
 def test_espocrm_sample():
-    driver = webdriver.Chrome()
+    driver = webdriver.Edge()
     driver.get("https://demo.us.espocrm.com")
     print(driver.title)
     print(driver.current_url)
@@ -20,5 +20,8 @@ def test_espocrm_sample():
     assert driver.current_url == "https://demo.us.espocrm.com/"
 
 
-# pytest -s Jan/ex02_Selenium_Basics/task01_11012025.py --alluredir=./allure-results
-# # allure serve allure-results
+# pytest -s main.py --alluredir=reports
+# allure serve reports
+
+
+
