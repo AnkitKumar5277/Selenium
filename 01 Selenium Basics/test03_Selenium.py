210a92a2c9839d39dba76795ffaf06d285342fb1
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-11-23 22:02:59 +0530

    Update test03_Selenium.py

--- 01 Selenium Basics/test03_Selenium.py
+++ 01 Selenium Basics/test03_Selenium.py
@@ -13,3 +13,17 @@ def test_vwo_login():
 
 # pytest main.py --alluredir=reports
 # allure serve reports
+
+from selenium import webdriver
+import allure
+import pytest
+
+@allure.title("verify that the title of vwo.com is expected")
+def test_vwo_sample():
+    driver = webdriver.Edge()
+    driver.get("https://app.vwo.com")
+    print(driver.title)
+    assert driver.title == "Login - VWO"
+
+# pytest main --alluredir=reports
+# allure serve reports
