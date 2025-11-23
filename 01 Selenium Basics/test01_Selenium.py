9e4e7c45db84447efa6cdedc9aaeab191ebec12f
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-11-23 22:22:38 +0530

    Update test01_Selenium.py

--- 01 Selenium Basics/test01_Selenium.py
+++ 01 Selenium Basics/test01_Selenium.py
@@ -28,16 +28,7 @@ def test_vwo_sample():
 # pytest main.py --alluredir=reports
 # allure serve reports
 
-from selenium import webdriver
-import allure
-import pytest
-import time
 
-def test_katalon_firefox():
-    driver = webdriver.Edge()
-    driver.get("https://katalon-demo-cura.herokuapp.com/")
-    time.sleep(10)
-    assert driver.current_url == "https://katalon-demo-cura.herokuapp.com/"
-    driver.quit()
+
 
 
