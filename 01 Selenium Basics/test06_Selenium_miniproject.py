c2af1002dd0404b54f5d86df154e6917ade00491
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-11-23 22:14:17 +0530

    Update test06_Selenium_miniproject.py

--- 01 Selenium Basics/test06_Selenium_miniproject.py
+++ 01 Selenium Basics/test06_Selenium_miniproject.py
@@ -6,10 +6,11 @@ import pytest
 def test_vwo_sample():
     driver = webdriver.Edge()
     driver.get("https://katalon-demo-cura.herokuapp.com/")
-    if "PURA Healthcare Service" in driver.page_source:
+    if "CURA Healthcare Service" in driver.page_source:
         print("Text Found!, test case passed")
     else:
         print("text not found on the page")
         pytest.fail("text not found on the page")
 
-# pytest Folder_1/test05_Selenium_Commands.py --alluredir=./allure-results
+# pytest main.py --alluredir=reports
+# allure serve reports
