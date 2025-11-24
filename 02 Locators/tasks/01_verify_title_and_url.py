9f89268fbe6114a3ba78c8d50d4c640d029447db
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2025-11-24 22:20:32 +0530

    Update and rename task01_verify_title_and_url.py to 01_verify_title_and_url.py

--- 02 Locators/tasks/01_verify_title_and_url.py
+++ 02 Locators/tasks/01_verify_title_and_url.py
@@ -1,30 +1,3 @@
-# import allure
-# import pytest
-# import time
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.chrome.options import Options
-#
-# @allure.title("Espo CRM Login Page.")
-# @allure.description("TC01-Positive Case-Verify that title and URL is appearing correctly.")
-# @pytest.mark.positiveespologinpage
-#
-# def test_load_espo_crm():
-#     chrome_options = Options()
-#     chrome_options.add_argument("--incognito")
-#     chrome_options.add_argument("--start-maximized")
-#
-#     driver = webdriver.Chrome(chrome_options)
-#     driver.get("https://demo.us.espocrm.com/")
-#     print(driver.title)
-#
-#     assert driver.title == "EspoCRM Demo"
-#     assert driver.current_url == ("https://demo.us.espocrm.com/")
-#
-#     time.sleep(5)
-#     driver.quit()
-
-
 import allure
 import pytest
 import time
@@ -49,4 +22,5 @@ def test_load_espo_crm():
     assert driver.current_url == "https://demo.us.espocrm.com/"
 
     time.sleep(5)
+
     driver.quit()
