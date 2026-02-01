d4ce18874965a6b1ad6ffb66d4d8012cb7115977
Ankit Kumar <121275934+AnkitKumar5277@users.noreply.github.com>
2026-02-01 20:37:00 +0530

    Delete 02 Locators directory

--- 02 Locators/b3_env_file.py
+++
@@ -1,9 +0,0 @@
-# Application URL
-URL=https://app.vwo.com
-
-# Invalid credentials for negative test
-INVALID_USERNAME=invalid_user@fakeemail.com
-INVALID_PASSWORD=wrongpassword123
-
-# Expected error message shown on the login page
-error_message_expected=Your email, password, IP address or location did not match
